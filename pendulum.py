cf6dc81b5ca91f2a0265e01c4c0f0de7a6572dbc
Ruslan Sevostyanov <zetoster@gmail.com>
2025-02-26 14:07:25 +0300

    added basic plots

--- pendulum.py
+++ pendulum.py
@@ -1,15 +1,25 @@
 import pybullet as p
 import time
 import pybullet_data
+import matplotlib.pyplot as plt
+import numpy as np
 
-dt = 1/240 # pybullet simulation step
-q0 = 0.0   # starting position (radian)
-qd = 1.5708   # desired position (radian)
-physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
+# physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
+physicsClient = p.connect(p.DIRECT) # or p.DIRECT for non-graphical version
+
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
 boxId = p.loadURDF("./pendulum.urdf", useFixedBase=True)
+
+dt = 1/240 # pybullet simulation step
+q0 = 0.0   # starting position (radian)
+qd = 1.5708   # desired position (radian)
+maxTime = 5
+logTime = np.arange(0, 5, dt)
+logPos = np.zeros(len(logTime))
+logVel = np.zeros(len(logTime))
+logCtrl = np.zeros(len(logTime))
 
 # get rid of all the default damping forces
 p.changeDynamics(boxId, 1, linearDamping=0, angularDamping=0)
@@ -23,13 +33,18 @@ p.changeDynamics(boxId, 2, linearDamping=0, angularDamping=0)
 # turn off the motor for the free motion
 p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetVelocity=0, controlMode=p.VELOCITY_CONTROL, force=0)
 
-kp = 20
-ki = 10
-kd = 5
+kp = 40
+ki = 40
+kd = 10
 err_int = 0.0
 err_prev = 0.0
-while True:
+for idx in range(len(logTime)):
     q = p.getJointState(boxId, 1)[0]
+    dq = p.getJointState(boxId, 1)[1]
+
+    logPos[idx] = q
+    logVel[idx] = dq
+
     err = q - qd
     # velocity control
     # propotional regulator is enough
@@ -43,7 +58,26 @@ while True:
     trq = -kp * err - ki * err_int - kd * err_diff
     err_prev = err
     p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, force=trq, controlMode=p.TORQUE_CONTROL)
+    logCtrl[idx] = trq
 
     p.stepSimulation()
-    time.sleep(dt)
+    # time.sleep(dt)
 p.disconnect()
+
+plt.subplot(3,1,1)
+plt.plot(logTime, logPos, label="pos")
+plt.plot([logTime[0],logTime[-1]], [qd, qd],'r--', label="ref")
+plt.grid(True)
+plt.legend()
+
+plt.subplot(3,1,2)
+plt.plot(logTime, logVel, label="vel")
+plt.grid(True)
+plt.legend()
+
+plt.subplot(3,1,3)
+plt.plot(logTime, logCtrl, label="ctrl")
+plt.grid(True)
+plt.legend()
+
+plt.show()
